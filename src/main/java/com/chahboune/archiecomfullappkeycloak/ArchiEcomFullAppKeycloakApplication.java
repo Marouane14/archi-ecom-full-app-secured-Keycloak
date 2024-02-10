187d547bfbede79ae940a17630cf029f42651d1f
Your Name <you@example.com>
2024-02-10 17:29:25 +0000

    FC

---
+++ src/main/java/com/chahboune/archiecomfullappkeycloak/ArchiEcomFullAppKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.chahboune.archiecomfullappkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiEcomFullAppKeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchiEcomFullAppKeycloakApplication.class, args);
+    }
+
+}
